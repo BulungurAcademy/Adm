3e2381c7f3f92dee839d5d0c0e45a5b83ea17e5b
Javohir <jumanovjavohir49@gmail.com>
2024-08-01 11:27:56 +0500

    changes add new base crud service

---
+++ Adm.Core/Services/BaseCrudService/IBaseCrudService.cs
@@ -0,0 +1,21 @@
+﻿using Adm.Core.BaseModels.BaseEntities;
+using System.Linq.Expressions;
+
+namespace Adm.Core.Services.BaseCrudService;
+public interface IBaseCrudService<TEntity, TPrimaryKey> where TEntity : class, IEntity<TPrimaryKey>
+{
+    Task<IQueryable<TEntity>> GetAllAsync();
+    Task<TEntity> GetAsync(TPrimaryKey id);
+    Task<TEntity> FirstOrDefaultAsync(TPrimaryKey id);
+    Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+
+    Task<TEntity> InsertAsync(TEntity entity);
+    Task<TPrimaryKey> InsertAndGetIdAsync(TEntity entity);
+
+    Task<TEntity> UpdateAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TPrimaryKey id, Func<TEntity, Task> updateAction);
+
+    Task DeleteAsync(TEntity entity);
+    Task DeleteAsync(TPrimaryKey id);
+    Task DeleteAsync(Expression<Func<TEntity, bool>> predicate);
+}
