e457a48e21ea959260d3134cb445237d13e7fd13
Javohir <jumanovjavohir49@gmail.com>
2024-08-08 11:45:59 +0500

    fixed that category service

--- Adm.Core/Services/IServiceBase.cs
+++ Adm.Core/Services/IServiceBase.cs
@@ -1,17 +1,17 @@
 ﻿namespace Adm.Core.Services;
 
-public interface IServiceBase<TEntity, in TPrimaryKey> : IQueryable<TEntity>
+public interface IServiceBase<T, in TId> : IQueryable<T>
 {
-    IQueryable<TEntity> GetAllAsQueryable(bool asNoTracking = false);
-    Task<TEntity?> GetByIdAsync(TPrimaryKey id, bool asNoTracking = false);
-    Task<TEntity> AddAsync(TEntity entity);
-    Task AddRangeAsync(params TEntity[] entities);
-    Task<TEntity> UpdateAsync(TEntity entity);
-    Task UpdateRangeAsync(params TEntity[] entities);
-    Task<TEntity> RemoveAsync(TEntity entity);
-    Task RemoveRangeAsync(params TEntity[] entity);
-    Task<bool> ExistsAsync(TPrimaryKey id);
-    Task ExistsOrThrowsNotFoundException(TPrimaryKey id);
-    Task<TEntity> GetByIdOrThrowsNotFoundException(TPrimaryKey id);
+    IQueryable<T> GetAllAsQueryable(bool asNoTracking = false);
+    Task<T?> GetByIdAsync(TId id, bool asNoTracking = false);
+    Task<T> AddAsync(T entity);
+    Task AddRangeAsync(params T[] entities);
+    Task<T> UpdateAsync(T entity);
+    Task UpdateRangeAsync(params T[] entities);
+    Task<T> RemoveAsync(T entity);
+    Task RemoveRangeAsync(params T[] entity);
+    Task<bool> ExistsAsync(TId id);
+    Task ExistsOrThrowsNotFoundException(TId id);
+    Task<T> GetByIdOrThrowsNotFoundException(TId id);
     Task<TValue?> GetNextSequenceValue<TValue>(string name, string? schemaName) where TValue : struct;
 }
